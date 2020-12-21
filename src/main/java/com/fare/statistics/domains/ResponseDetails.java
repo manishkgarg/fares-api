c85e18adb7124ab1c15b70accb3d7532857f7270
manishkgarg <manish_murthal@yahoo.co.in>
2020-12-21 20:54:42 +0530

    removed unnecessary getters & setters

--- src/main/java/com/fare/statistics/domains/ResponseDetails.java
+++ src/main/java/com/fare/statistics/domains/ResponseDetails.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @Builder
-@Document(collection="ResponseDetails")
+@Document(collection = "ResponseDetails")
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResponseDetails {
@@ -27,36 +27,5 @@ public class ResponseDetails {
 	public String UUID;
 	public int statusCode;
 	public int responseTime;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getApiName() {
-		return apiName;
-	}
-	public void setApiName(String apiName) {
-		this.apiName = apiName;
-	}
-	public String getUUID() {
-		return UUID;
-	}
-	public void setUUID(String uUID) {
-		UUID = uUID;
-	}
-	public int getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	public int getResponseTime() {
-		return responseTime;
-	}
-	public void setResponseTime(int responseTime) {
-		this.responseTime = responseTime;
-	}
-	
+
 }
--- src/main/java/com/fare/statistics/models/AccessTokenResponse.java
+++ src/main/java/com/fare/statistics/models/AccessTokenResponse.java
@@ -1,12 +1,20 @@
 package com.fare.statistics.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * This Model is to bind token response generated from mocked oauth URL.
+ * 
  * @author Manish
  *
  */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccessTokenResponse {
 
 	private String access_token;
